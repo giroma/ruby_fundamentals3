a5ef4b3ee1469330792bd7ff5a31a6bbd6b95d54
Argir Argirov <dj.giro@gmail.com>
2017-09-20 17:12:10 -0400

    made arrays and hashes

---
+++ exercises.rb
@@ -0,0 +1,31 @@
+fav_colors = ["blue", "purple", "cyan", "wine"]
+age = [41, 25, 30 ,64, 30]
+coin = ["heads", "heads", "tails", "heads", "tails"]
+artists = ["drake", "queen", "bob dylan"]
+colors = [:blue, :purple, :cyan, :wine]
+
+
+dictionary = {
+  :clean => "has no blemishes",
+  boss: "someone who oversees others",
+  pc: "personal computer devise"
+}
+
+movies = {
+  "there will be blood" => 2005,
+  "silent bob" => 2000,
+  "Die Hard" => 1985
+}
+
+cities = {
+  toronto: 3453675,
+  sofia: 1453000,
+  "San Franscisco"
+}
+
+names = {
+  ari: 24,
+  cait: 31,
+  mark: 41,
+  richard: 53
+}
