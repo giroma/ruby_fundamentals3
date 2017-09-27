aa769039ffe5bf9ce1aaf8281a15646fb00e597f
Argir Argirov <dj.giro@gmail.com>
2017-09-27 15:36:16 -0400

    odd_sum, and .include? done

---
+++ reinforcement_sep27.rb
@@ -0,0 +1,21 @@
+# a = [1,3,6,46,433,234,78,88,34,33]
+#
+#
+# def odd_sum(a)
+#   odd = a.select{|num| num.odd?}
+#   return odd.sum
+# end
+# puts a
+# puts odd_sum(a)
+
+
+names = ['Ari', 'Cait', 'Lu']
+
+puts 'Enter name:'
+name = gets.chomp
+
+if names.include?(name)
+ puts "Hey #{name}"
+else
+  puts "Who goes there?"
+end
