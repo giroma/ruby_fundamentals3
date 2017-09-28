465645295d04ca05588fe1d371b26b98215c24ce
Argir Argirov <dj.giro@gmail.com>
2017-09-28 13:20:01 -0400

    update

--- reinforcement_sep27.rb
+++ reinforcement_sep27.rb
@@ -1,12 +1,12 @@
-# a = [1,3,6,46,433,234,78,88,34,33]
-#
-#
-# def odd_sum(a)
-#   odd = a.select{|num| num.odd?}
-#   return odd.sum
-# end
-# puts a
-# puts odd_sum(a)
+a = [1,3,6,46,433,234,78,88,34,33]
+
+
+def odd_sum(a)
+  odd = a.select{|num| num.odd?}
+  return odd.sum
+end
+puts a
+puts odd_sum(a)
 
 
 names = ['Ari', 'Cait', 'Lu']
